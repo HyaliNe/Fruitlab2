e4bad28444995ab953f9cf6b2cf76fab92a85b2d
Alvin <defalcaror@gmail.com>
2012-10-01 13:07:23 +0800

    commit before adding validation method

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -9,6 +9,7 @@ class Upload extends CI_Controller {
 	function index()
 	{
 		$this->load->view('upload_form', array('error' => ' ' ));
+             
 	}
 
 	function do_upload()
@@ -18,19 +19,20 @@ class Upload extends CI_Controller {
 		$config['max_size']	= '1000';
 		$config['max_width']  = '1024';
 		$config['max_height']  = '768';
-
 		$this->load->library('upload', $config);
 
 		if ( ! $this->upload->do_upload())
 		{
 			$error = array('error' => $this->upload->display_errors());
-
 			$this->load->view('upload_form', $error);
 		}
 		else
 		{
+                        $this->library->load("validation");
+                        $rules = array(
+                              
+                            );
 			$data = array('upload_data' => $this->upload->data());
-
 			$this->load->view('upload_success', $data);
 		}
 	}
