12a9143597dd46a726a8a52586d2005200ab54a6
Alvin <defalcaror@gmail.com>
2012-10-01 13:34:23 +0800

    commit before calling main method

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -1,13 +1,20 @@
 <html>
 <head>
-<title>Upload Form</title>
+<title>Upload A Design</title>
 </head>
 <body>
 <?php echo $error;?>
 <?php echo form_open_multipart('upload/do_upload');?>
-<input type=""
-<input type="file" name="userfile" size="20" />
-<br /><br />
+<label for="title">Title:</label><input type="text" name="title">
+<br/>
+<label for="price">Price:</label><input type="text" name="price">
+<br/>
+<label for="type">Type:</label><input type="text" name="type">    
+<br/>
+<label for="rating">Rating:</label><input type="text" name="rating">
+<br/>
+<label for="image_path">Upload Design</label><input type="file" name="image_path" size="20" />
+<br />
 <input type="submit" value="upload" />
 </form>
 </body>
