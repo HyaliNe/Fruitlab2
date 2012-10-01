f56f19e64321861a797a3d3da81c81e407671510
Alvin <defalcaror@gmail.com>
2012-10-01 14:31:25 +0800

    commit before making changes

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -8,42 +8,32 @@ class Upload extends CI_Controller {
 	}
 	function index()
 	{       
-                $this->load->library('form_validation');
-		$this->form_validation->set_rules('title', 'title', 'required|trim');
-		$this->form_validation->set_rules('price', 'price', 'required|trim');
-		$this->form_validation->set_rules('type', 'type', 'required|trim');
-                $this->form_validation->set_rules('rating','rating','required|trim');
-                if($this->form_validation->run()){
-                    
-                }
-                $data['error'] = '';
-                $data['main_content'] = 'upload_form';
-		$this->load->view('includes/template', $data);
+            //configurations files for uploading images
+            $config['upload_path'] = './uploads/';
+	    $config['allowed_types'] = 'gif|jpg|png';
+	    $config['max_size']	= '1000';
+	    $config['max_width']  = '1024';
+	    $config['max_height']  = '768';
+            $this->load->library('upload', $config);
+            $this->load->library('form_validation');
+            $this->form_validation->set_rules('title', 'title', 'required|trim');
+	    $this->form_validation->set_rules('price', 'price', 'required|trim');
+	    $this->form_validation->set_rules('type', 'type', 'required|trim');
+            $this->form_validation->set_rules('rating','rating','required|trim');
+            $data['error'] = '';
+            $data['main_content'] = 'upload_form';
+            $result = $this->upload->do_upload();
+            if($this->form_validation->run()){
+                 print_r($this->input->post());
+                 if( !$result){//upload image failures
+                     $data['error'] = 'The images is not allowed';
+                 }else{    
+                     $data['error'] = 'upload_success';
+                     $data = array('upload_data' => $this->upload->data());
+                 }
+            }
+            $this->load->view('includes/template', $data);
         }
-	function do_upload()
-	{
-                
-		$config['upload_path'] = './uploads/';
-		$config['allowed_types'] = 'gif|jpg|png';
-		$config['max_size']	= '1000';
-		$config['max_width']  = '1024';
-		$config['max_height']  = '768';
-		$this->load->library('upload', $config);
-
-		if ( ! $this->upload->do_upload())
-		{
-			$error = array('error' => $this->upload->display_errors());
-			$this->load->view('upload_form', $error);
-		}
-		else
-		{
-                        $this->library->load("validation");
-                        $rules = array(
-                            );
-			$data = array('upload_data' => $this->upload->data());
-			$this->load->view('upload_success', $data);
-		}
-	}
 }
 
 /* End of file account.php */
