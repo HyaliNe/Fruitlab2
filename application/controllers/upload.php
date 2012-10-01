82e54bbbc4c0e0eaddd0c6d0a7b3fb37a1fe76a7
Alvin <defalcaror@gmail.com>
2012-10-01 14:34:47 +0800

    added back do_upload method

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -25,15 +25,35 @@ class Upload extends CI_Controller {
             $result = $this->upload->do_upload();
             if($this->form_validation->run()){
                  print_r($this->input->post());
-                 if( !$result){//upload image failures
-                     $data['error'] = 'The images is not allowed';
-                 }else{    
-                     $data['error'] = 'upload_success';
-                     $data = array('upload_data' => $this->upload->data());
-                 }
             }
             $this->load->view('includes/template', $data);
         }
+        
+        
+        function do_upload()
+	{
+		$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['max_size']	= '1000';
+		$config['max_width']  = '1024';
+		$config['max_height']  = '768';
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload())
+		{
+			$error = array('error' => $this->upload->display_errors());
+			$this->load->view('upload_form', $error);
+		}
+		else
+		{
+                        $this->library->load("validation");
+                        $rules = array(
+                              
+                            );
+			$data = array('upload_data' => $this->upload->data());
+			$this->load->view('upload_success', $data);
+		}
+	}
 }
 
 /* End of file account.php */
