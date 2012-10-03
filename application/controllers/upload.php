e5282884191fc1fb0b3538559490797d512a1edb
Alvin <defalcaror@gmail.com>
2012-10-03 11:08:46 +0800

    upload files

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -48,26 +48,6 @@ class Upload extends CI_Controller {
                 $this->load->view('includes/template', $data);
             }
         }
-        function do_upload()
-	{
-		$config['upload_path'] = './uploads/';
-		$config['allowed_types'] = 'gif|jpg|png';
-		$config['max_size']	= '1000';
-		$config['max_width']  = '1024';
-		$config['max_height']  = '768';
-		$this->load->library('upload', $config);
-		if (!$this->upload->do_upload())
-		{
-			$error = array('error' => $this->upload->display_errors());
-			$this->load->view('upload_form', $error);
-		}
-		else
-		{       
-			$data = array('upload_data' => $this->upload->data());
-			$this->load->view('upload_success', $data);
-		}
-
-        }
 
 /* End of file account.php */
 /* Location: ./controllers/account.php */	
