d1a655104ac6f7eb86302383fa71029995819633
Alvin <defalcaror@gmail.com>
2012-10-03 13:12:16 +0800

    before using the views files to queries the record

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -12,17 +12,19 @@
 		<?php echo validation_errors(); ?>
 		</div>
     <?php endif ?>
+    
 <label for="title">Title:</label><input type="text" name="title" value="<?php echo set_value('title'); ?>">
 <br/>
 <label for="price">Price:</label><input type="text" name="price" value="<?php echo set_value('price'); ?>">
 <br/>
-<label for="type">Type:</label><input type="text" name="type" value="<?php echo set_value('type'); ?>">    
+Sales:<input type="radio" name="type" value="Sales" <?php echo set_radio('type', 'On Sales', TRUE); ?> />
+Private:<input type="radio" name="type" value="Private" <?php echo set_radio('type', 'Private'); ?> />
 <br/>
-<label for="rating">Rating:</label><input type="text" name="rating" value="<?php echo set_value('rating')?>">
-<br/>
-<label for="image_path">Upload Design</label><input type="file"  value="<?php echo set_value('userfile')?>" name="userfile" size="20" />
+<label for="image_path">Upload Design:</label><input type="file" value="<?php echo set_value('userfile')?>" name="userfile" size="20" />
 <br />
-<input type="submit" value="Upload Design" />
+<input type="submit" value="Submit" />
 </form>
 </body>
+<!-- to add to the models to others-->
+
 </html>
