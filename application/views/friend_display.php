ade82ba0ea4c5e0115f0ab816f0a2dd96c0f6564
Alvin <defalcaror@gmail.com>
2012-10-04 13:59:47 +0800

    mistakes on friend_displays before adding the echo form values

---
+++ application/views/friend_display.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Upload A Design</title>
+</head>
+<body>
+<?php echo $error;?>
+<?php echo form_open('friend/search')?>
+<label for="fname">Name</label><input type="fname">
+<label for="username">UserName</label>
+<label for="age">Age</label>
+</form>
+
+<table border = '1'> 
+<tr><th>Title</th><th>Image_Path</th><th>Price($)</th><th>Type</th><th>Design_ID</th><th>Action</th></tr>
+
+
+</table>
+</body>
+</html>
