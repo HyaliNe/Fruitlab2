4b4b8cfbf939c3bc332aecc25d69a9111d322ba9
Alvin <defalcaror@gmail.com>
2012-10-04 17:03:54 +0800

    currently the search by email not working

--- application/views/friend_display.php
+++ application/views/friend_display.php
@@ -2,20 +2,63 @@
 <head>
 <title>Upload A Design</title>
 </head>
+<link rel="stylesheet" href="js/Alvin/themes/base/jquery.ui.all.css">
+	<script src="js/Alvin/jquery-1.7.1.js"></script>
+	<script src="js/Alvin/ui/jquery.ui.core.js"></script>
+	<script src="js/Alvin/ui/jquery.ui.widget.js"></script>
+	<script src="js/Alvin/ui/jquery.ui.datepicker.js"></script>
+<script>
+	 $(function() {
+		//must be placed at the top of the block
+		var dates = $( "#fromage, #toage" ).datepicker({
+			defaultDate: "+1w",
+			changeMonth: true,
+		    changeYear : true,
+			yearRange: '1921:' + new Date().getFullYear(),
+			numberOfMonths: 1,
+			onSelect: function( selectedDate ) {
+				var option = this.id == "fromage" ? "minDate" : "maxDate",
+					instance = $( this ).data( "datepicker" ),
+					date = $.datepicker.parseDate(
+						instance.settings.dateFormat ||
+						$.datepicker._defaults.dateFormat,
+						selectedDate, instance.settings );
+				dates.not( this ).datepicker( "option", option, date );
+			}
+		});
+		$( "#fromage,#toage" ).datepicker( "option", "dateFormat", "yy/mm/dd");
+		//$("#startdate").datepicker('setDate', new Date());//new line of code to start code
+		//$("#enddate").datepicker('setDate', new Date());//new line of code to start code
+	});
+
+</script>
 <body>
+    <h2>Search for Friends</h2>
 <?php echo $error;?>
-<?php echo form_open('friend/search')?>
-<label for="fname">Name</label><input type="text" name="fname" value="<?php echo set_value('title'); ?>"/>
-<label for="username">UserName</label><input type="text" name="username" value="<?php echo set_value('username'); ?>"/>
-<label for="age">Age</label><input type="text" name="age" size="2" maxlength="2" value="<?php echo set_value('age'); ?>"/>
+<?php echo form_open('friends/search')?>
+<label for="fname">Name:</label><input type="text" name="fname" value="<?php echo set_value('title'); ?>"/>
+<label for="email">Email:</label><input type="text" name="email" value="<?php echo set_value('email'); ?>"/>
+<label for="fromage">From Age:</label><input type="text" id="fromage" name="fromage" value="<?php echo set_value('fromage'); ?>"/>
+<label for="toage">To Age:</label><input type="text" id="toage" name="toage" value="<?php echo set_value('toage'); ?>"/>
 <br/>
-Male:<input type="radio" name="type" value="Sales" <?php echo set_radio('type', 'On Sales'); ?> />
-Female:<input type="radio" name="type" value="Private" <?php echo set_radio('type', 'Private'); ?> />
+Male:<input type="radio" name="gender" value="male" <?php echo set_radio('gender', 'male'); ?> />
+Female:<input type="radio" name="gender" value="female" <?php echo set_radio('gender', 'female'); ?> />
+<br/>
 <br/>
 <input type="submit" value="Submit"><input type="Reset">
 </form>
+
 <table border = '1'> 
 <tr><th>Name</th><th>UserName</th><th>Age</th><th>Gender</th></tr>
+<?php
+foreach($resultset->result() as $row){
+        echo "<tr>";
+	echo "<td>".$row->first_name."</td><td>".$row->email ."</td>";
+	echo "<td>".$row->gender."</td><td>".$row->date_of_birth ."</td>";
+	echo "</tr>";
+}
+?>
 </table>
 </body>
 </html>
+
