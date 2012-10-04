dd3b091c980aaa14a19b1120d50ee19745ed38c2
Alvin <defalcaror@gmail.com>
2012-10-04 16:26:25 +0800

    friends_display_already_workign

--- application/controllers/friends.php
+++ application/controllers/friends.php
@@ -7,18 +7,36 @@ class Friends extends CI_Controller {
 	}
 	function index()
 	{  
-            //$get current friends based on user id
+            $data['searchvar'] = array();
             $data["error"] = '';
             $data["main_content"]  = 'friend_display';
             $this->load->view('includes/template', $data);
         }
         function search(){
+            //$this->db->query
             $data["error"] = '';
             $data["main_content"]  = 'friend_display';
             $postdata = $this->input->post();
-            $name = $postdata['fname'];
-            $username = $postdata['username'];
-            $age = $postdata['age'];
+            if(isset($postdata['fname']) && $_POST['fname'] != ''){
+                $name = $_POST['fname'];
+                $this->db->where(array('first_name' => $name));
+            }
+            if(isset($postdata['username']) && $_POST['username'] != ''){
+                $username = $_POST['username'];
+                $this->db->where(array('username' => $username));
+            }
+            if((isset($postdata['gender']) && $_POST['gender']) != ''){
+                 $gender = $postdata['gender'];
+                 $this->db->where(array('gender' => $gender));
+            }
+            if((isset($postdata['fromage']) && $postdata['fromage'] != '' && $postdata['toage'] && $postdata['toage'] !='')){
+                    $fromage = $postdata['fromage'];
+                    $toage = $postdata['toage'];
+                    $this->db->where("YEAR(CURDATE()) - dob_year - IF( STR_TO_DATE( CONCAT( YEAR( CURDATE( ) ) ,  '-', dob_month,  '-', dob_day ),'%Y-%c-%e' )> CURDATE( ) , 1, 0 ) BETWEEN '$fromage' AND '$toage'");
+            }
+            $searchvar = $this->db->get("customer");
+            $data['searchvar'] = $searchvar;
+            $data['main_content'] = 'friend_display';
             $this->load->view('includes/template', $data);
         }
 }
