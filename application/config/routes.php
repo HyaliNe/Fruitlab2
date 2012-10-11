21c3925047ec67f29bdb29780b1f76da674bf44e
Chen Deshun <Deshun@xquisitelabs.com>
2012-10-11 17:07:26 +0800

    added route for user profiles partiall completed profile method for guoliang. update gitignore to recurvisely ignore all .DS_store files

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,6 +41,7 @@
 $route['default_controller'] = "home";
 $route['404_override'] = '';
 $route['design/(:num)'] = 'design/singleDesign/$1';
+$route['user/(:num)'] = 'profile/profile/$1'
 
 
 /* End of file routes.php */
