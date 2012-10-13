073cdbe4bc938ccbf181155d72655e8eebe48aab
Guo Liang <gguoliang89@gmail.com>
2012-10-13 14:24:52 +0800

    edited settings

--- application/views/account/setting.php
+++ application/views/account/setting.php
@@ -1,23 +1,3 @@
-<head>
-
-<script>
-function enableform()
-{
-document.getElementById("first_name").readOnly=false;
-document.getElementById("last_name").readOnly=false;
-document.getElementById("country").readOnly=false;
-document.getElementById("date_of_birth").readOnly=false;
-document.getElementById("gender").readOnly=false;
-document.getElementById("hp_no").readOnly=false;
-document.getElementById("about_you").readOnly=false;
-document.getElementById("oldpassword").readOnly=false;
-document.getElementById("password").readOnly=false;
-document.getElementById("password2").readOnly=false;
-}
-</script>
-
-</head>
-
 <div class="container">
 	
 	<div class = "page-header">
@@ -44,7 +24,7 @@ document.getElementById("password2").readOnly=false;
 						<label for="first_name">First name</label>					
 					</div>
 					<div class="controls">
-						<input type="text" name = "first_name" id="first_name" readonly="readonly" value = "<?php echo set_value('first_name', $first_name); ?>" />					
+						<input type="text" name = "first_name" id="first_name" value = "<?php echo set_value('first_name', $first_name); ?>" />					
 					</div>
 				</div> <!-- end of .control-group -->					
 			</div> <!-- end of .span3 -->
@@ -55,18 +35,10 @@ document.getElementById("password2").readOnly=false;
 						<label for="last_name">Last name</label>					
 					</div>
 					<div class="controls">
-						<input type="text" name = "last_name" id="last_name" readonly="readonly" value = "<?php echo set_value('last_name', $last_name); ?>" />					
+						<input type="text" name = "last_name" id="last_name" value = "<?php echo set_value('last_name', $last_name); ?>" />					
 					</div>
 				</div> <!-- end of .control-group -->					
 			</div> <!-- end of .span3 -->
-			
-			<div class="span3">
-				<div class="control-group">
-					<div class="control-label">
-						<button type="button" class="btn btn-primary" onClick="enableform()" id="edit" name="edit">Edit</button>
-					</div>
-				</div>
-			</div>
 		</div> <!-- end of .row -->
 
 
@@ -76,7 +48,7 @@ document.getElementById("password2").readOnly=false;
 				<div class="control-group">
 			      <label class="control-label" for="email">Email</label>
 			      <div class="controls">
-			        <input type="text" id="email" readonly="readonly" name = "email" value = "<?php echo $email; ?>" />	<!-- email should not be allowed to change, if want to change, can create new account -->
+			        <input type="text" id="email" name = "email" readonly="readonly" value = "<?php echo $email; ?>" />	<!-- email should not be allowed to change, if want to change, can create new account -->
 			      </div>
 				</div> <!-- end of .control-group -->
 			</div> <!-- end of .span3 -->
@@ -85,7 +57,7 @@ document.getElementById("password2").readOnly=false;
 				<div class="control-group">
 					<label class="control-label" for="country">Country</label>
 			     	<div class="controls">
-						<input name="country" readonly="readonly" id="country" <?php echo country_dropdown('country', $country); ?>
+						<?php echo country_dropdown('country', $country); ?>
 			         </div> <!-- .controls -->
 				 </div> <!-- .control-group -->				
 			</div> <!-- end of .span3 -->
@@ -98,7 +70,7 @@ document.getElementById("password2").readOnly=false;
 				<div class="control-group">
 			      <label class="control-label" for="date_of_birth">Date of birth</label>
 			      <div class="controls">
-			        <input type="text" id="date_of_birth" readonly="readonly" name = "date_of_birth" value = "<?php echo $date_of_birth; ?>" />
+			        <input type="text" id="date_of_birth" name = "date_of_birth" value = "<?php echo $date_of_birth; ?>" />
 			      </div>
 				</div> <!-- end of .control-group -->
 			</div> <!-- end of .span3 -->
@@ -110,18 +82,18 @@ document.getElementById("password2").readOnly=false;
 			      <div class="controls">
 					<?php	if($gender == 'M')
 							{	?>
-								<input type="radio" id="gender" readonly="readonly" name="gender" value="M" checked="checked">Male
-								<input type="radio" id="gender" readonly="readonly" name="gender" value="F">Female					
+								<input type="radio" id="gender" name="gender" value="M" checked="checked">Male
+								<input type="radio" id="gender" name="gender" value="F">Female					
 					<?php	}	
 							elseif($gender == "F")
 							{	?>
-								<input type="radio" id="gender" readonly="readonly" name="gender" value="M" >Male
-								<input type="radio" id="gender" readonly="readonly" name="gender" value="F" checked="checked">Female					
+								<input type="radio" id="gender" name="gender" value="M" >Male
+								<input type="radio" id="gender" name="gender" value="F" checked="checked">Female					
 					<?php	}
 							else
 							{	?>
-								<input type="radio" id="gender" readonly="readonly" name="gender" value="M">Male
-								<input type="radio" id="gender" readonly="readonly" name="gender" value="F">Female				
+								<input type="radio" id="gender" name="gender" value="M">Male
+								<input type="radio" id="gender" name="gender" value="F">Female				
 					<?php	}	?>			        
 			      </div>
 				</div> <!-- end of .control-group -->
@@ -134,7 +106,7 @@ document.getElementById("password2").readOnly=false;
 				<div class="control-group">
 			      <label class="control-label" for="hp_no">Handphone no</label>
 			      <div class="controls">
-			        <input type="text" id="hp_no" name = "hp_no" readonly="readonly" value = "<?php echo $hp_no; ?>" />
+			        <input type="text" id="hp_no" name = "hp_no" value = "<?php echo $hp_no; ?>" />
 			      </div>
 				</div> <!-- end of .control-group -->
 			</div> <!-- end of .span3 -->			
@@ -144,7 +116,7 @@ document.getElementById("password2").readOnly=false;
 				<div class="control-group">
 			      <label class="control-label" for="about_you">About you</label>
 			      <div class="controls">
-			        <textarea cols="40" id="about_you" readonly="readonly" name="about_you" rows="2" maxlength="150" value= "<?php echo set_value('about_you',$about_you); ?>"><?php echo $about_you;?></textarea>
+			        <textarea cols="40" id="about_you" name="about_you" rows="2" maxlength="150" value= "<?php echo set_value('about_you',$about_you); ?>"><?php echo $about_you;?></textarea>
 			      </div>
 				</div> <!-- end of .control-group -->
 			</div> <!-- end of .span3 -->
@@ -157,7 +129,7 @@ document.getElementById("password2").readOnly=false;
 						<label for="password">Old password</label>					
 					</div>
 					<div class="controls">
-						<input type="password" name = "oldpassword" id="oldpassword" readonly="readonly" value = "<?php echo set_value('oldpassword'); ?>" />
+						<input type="password" name = "oldpassword" id="oldpassword" value = "<?php echo set_value('oldpassword'); ?>" />
 						<input type="hidden" name="dbpassword" value="<?php echo $password; ?>" />
 						<?php	if(!$result)
 								{
@@ -176,7 +148,7 @@ document.getElementById("password2").readOnly=false;
 						<label for="password">New password</label>					
 					</div>
 					<div class="controls">
-						<input type="password" name = "password" id="password" readonly="readonly" value = "<?php echo set_value('password'); ?>" />					
+						<input type="password" name = "password" id="password" value = "<?php echo set_value('password'); ?>" />					
 					</div>
 				</div> <!-- end of .control-group -->					
 			</div> <!-- end of .span3 -->
@@ -187,7 +159,7 @@ document.getElementById("password2").readOnly=false;
 						<label for="password2">Confirm Password</label>					
 					</div>
 					<div class="controls">
-						<input type="password" name = "password2" id="password2" readonly="readonly" value = "<?php echo set_value('password2'); ?>" />					
+						<input type="password" name = "password2" id="password2" value = "<?php echo set_value('password2'); ?>" />					
 					</div>
 				</div> <!-- end of .control-group -->					
 			</div> <!-- end of .span3 -->
