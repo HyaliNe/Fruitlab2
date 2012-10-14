4a6b764f60009c24b4dbad2f40452d071cdacebb
Alvin <defalcaror@gmail.com>
2012-10-14 21:54:11 +0800

    with somebugs

--- application/views/transaction/paymenthistory.php
+++ application/views/transaction/paymenthistory.php
@@ -1,7 +1,14 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<html>
+    <body>
+    <table border="1">
+    <tr><th>Title</th><th>Price</th><th>Quantity</th><th>Image</th><th>Status</th></tr>
+    <?php
+    foreach($sales as $s){
+      ?>
+     <tr><td><?=$s['title']?></td><td><?=$s['price']?></td><td><?=$s['quantity']?></td><td><img src='<?=$s['image_path']?>' width='100' height='100'/></td><td><?=$s['status']?></td></tr>
+     <?
+    }
+    ?>
+    </table>    
+    </body>
+</html>
