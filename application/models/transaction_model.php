3d60f9ddbcf16cfec412611ad131af0806cae3c8
Alvin <defalcaror@gmail.com>
2012-10-26 17:43:43 +0800

    removed cart related functions

--- application/models/transaction_model.php
+++ application/models/transaction_model.php
@@ -135,49 +135,5 @@ class transaction_model extends CI_Model {
         }
         return $result;
     }
-    
-
-    public function getCartByID($id){
-         $result = array();
-         $result['result'] = FALSE;
-         $this->db->select("cart_id,date,status,reference");
-         $this->db->where("cart_id",1);
-         $query = $this->db->get('cart',1);
-         if($query->num_rows == 1)
-		{
-			//storing the row return from the query
-			$row = $query->row();
-			$result['cart_id'] = $row->cart_id;
-			$result['date'] = $row->first_name;
-			$result['status'] = $row->last_name;
-			$result['reference'] = $row->reference;		//need not query since already available
-                        $result['result'] = TRUE;
-                }
-          return $result;
-     }      
-
-    
-    
- 
-     public function getSingleLineItemByID($id){
-         $result = array();
-         $result['result'] = FALSE;
-         $this->db->select("singlelineitem,design_id,quantity,collar_id,material_id,colour_id");
-         $this->db->where("singlecartitem",1);
-         $query = $this->db->get("cartlist");
-         if($query->num_rows == 1)
-		{
-                        //storing the row return from the query
-			$row = $query->row();
-			$result['singlelineitem'] = TRUE;
-			$result['design_id'] = $row->first_name;
-			$result['quantity'] = $row->last_name;
-			$result['collar_id'] = $row->reference;		//need not query since already available
-                        $result['material_id'] = $row->reference;
-                        $result['colour_id'] = $row->reference;//need not query since already availabl
-                        $result['result'] = TRUE;
-                }
-          return $result;
-     }
 }
 ?>
