0ae4a4dc8e335b5e0042f77775aba50689b26ec8
Alvin <defalcaror@gmail.com>
2012-10-26 19:49:37 +0800

    fixed to -1 statement

--- application/views/transaction/purchasestatement.php
+++ application/views/transaction/purchasestatement.php
@@ -1,30 +1,25 @@
 <html>
     <body>
     <h2>Purchase Statement</h2>
-   
     <?php
     foreach($purchases as $s){
-         print_r($s);
      ?>
     <table border="1">
     <tr><th>Cart_ID</th><th>Date</th><th>Status</th><th>Reference</th></tr>
-    <tr><td><?=$s['cart_id']?></td><td><?=$s['date']?></td><td><?=$s['status']?></td><td><?=$s['reference_no']?></td></tr>
+    <tr><td><?=$s['cart_id']?></td><td><?=$s['date']?></td><td><?=$s['status']?></td><td><?=$s['reference_number']?></td></tr>
     </table>
     <br/>
-    <?php
-    ?>
     <table border="1">
         <tr><th>Title</th><th>Price</th><th>Quantity</th></tr>
-    <?php
-        for($i = 0; $i < sizeof($s['singlelineitem']);$i++){ 
+    <?php   
+        for($i = 0; $i < sizeof($s) - 1 ;$i++){ 
         $k = $s['singlelineitem'][$i];
-   ?>
+    ?>
         <tr><td><?=$k['title']?></td><td><?=$k['price']?></td><td><?=$k['quantity']?></td></tr>
     <?php
         }
     ?>
     </table>
-    <br/>
     <?php
     }
     ?>
